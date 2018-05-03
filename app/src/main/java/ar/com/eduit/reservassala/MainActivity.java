0c8100fceed8e41a4c16349f9da1b503a8f69731
HerZar <hernanzarzycki@gmail.com>
2018-05-03 16:25:28 -0300

    Version 0.2.1 creacion del branch para cambiar el menu de opciones.

--- app/src/main/java/ar/com/eduit/reservassala/MainActivity.java
+++ app/src/main/java/ar/com/eduit/reservassala/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        // este mensaje es para ver que el branch es correcto.
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
